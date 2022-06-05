70738f08face9fffdbc83f4bfe5f8848d8a945ab
SpreeHertz <spreehertzofficial@gmail.com>
2022-06-05 16:35:18 +0530

    feat: add fs and /savelocation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { PlayerChatEvent, ServerCommands, ServerEvents } from '@customrealms/core';
+import { PlayerChatEvent, ServerCommands } from '@customrealms/core';
+import fs from 'fs';
 
+ServerCommands.register(`/savelocation {location}...`, async (player, call) => {
 
-ServerCommands.register(`/myuuid`, (player, call) => {
-    const uuid = player.getUUID();
+     const locationX = player.getTargetBlock()?.getX();
+     const locationY = player.getTargetBlock()?.getY();
+     const locationZ = player.getTargetBlock()?.getZ();
 
-    player.sendMessage(`Your uuid is ${uuid}`);
+     const locationName = call.getPlaceholder('location')!;
+
+     const parsedJSON = JSON.parse(locationName + locationX + locationY + locationZ);
+
+     fs.writeFile(`data/locations.json`, parsedJSON, 'utf-8', (err) => {
+          if (err) throw err;
+     })
+
+     console.log(`${player.getDisplayName()} has saved a location named "${locationName}". X: ${locationX} / Y: ${locationY} / Z: ${locationZ}`)
+
+     player.sendMessage(`Your location named "${locationName}" has been saved.\nX: ${locationX} / Y: ${locationY} / Z: ${locationZ}`)
 })
